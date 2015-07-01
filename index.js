3ec9131b8f367317ff6a167e63621ad8b22a8121
Mathieu M-Gosselin <mathieumg@gmail.com>
2015-07-01 02:26:30 -0400

    Added support for https://github.com/leebyron/ecmascript-more-export-from#proposed-additions in block-scoped-var rule.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 module.exports = {
   rules: {
+    'block-scoped-var': require('./rules/block-scoped-var'),
     'object-shorthand': require('./rules/object-shorthand'),
     'generator-star-spacing': require('./rules/generator-star-spacing'),
     'generator-star': require('./rules/generator-star')
   },
   rulesConfig: {
+    'block-scoped-var': 0,
     'generator-star-spacing': 0,
     'generator-star': 0,
     'object-shorthand': 0
