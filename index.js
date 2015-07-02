c052e7d67afa266070a8f74d2a55cd99413ecbb4
Mathieu M-Gosselin <mathieumg@gmail.com>
2015-07-02 01:20:20 -0400

    Added support for experimental exports in the `space-in-brackets` rule.

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ module.exports = {
     'object-shorthand': require('./rules/object-shorthand'),
     'generator-star-spacing': require('./rules/generator-star-spacing'),
     'generator-star': require('./rules/generator-star'),
-    'new-cap': require('./rules/new-cap')
+    'new-cap': require('./rules/new-cap'),
+    'space-in-brackets': require('./rules/space-in-brackets'),
   },
   rulesConfig: {
     'generator-star-spacing': 0,
     'generator-star': 0,
     'object-shorthand': 0,
-    'new-cap': 0
+    'new-cap': 0,
+    'space-in-brackets': 0
   }
 };
