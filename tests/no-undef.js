e611434460efaa8037ea00d22f63a18bb1bcceac
Marten Schilstra <mail@martenschilstra.nl>
2015-11-13 11:24:20 -0500

    Add allow-decorator-macro rule as an exception to no-undef

---
+++ tests/no-undef.js
@@ -0,0 +1,82 @@
+/* eslint-disable */
+
+/**
+ * @fileoverview Tests for no-undef rule.
+ * @author Mark Macdonald
+ */
+
+var rule = require('../rules/no-undef'),
+    RuleTester = require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-undef", rule, {
+    valid: [
+        // Original test cases.
+        "var a = 1, b = 2; a;",
+        "/*global b*/ function f() { b; }",
+        { code: "function f() { b; }", globals: { b: false} },
+        { code: "function f() { b; }", global: { b: false} },
+        "/*global b a:false*/  a;  function f() { b; a; }",
+        "function a(){}  a();",
+        "function f(b) { b; }",
+        "var a; a = 1; a++;",
+        "var a; function f() { a = 1; }",
+        "/*global b:true*/ b++;",
+        "/*eslint-env browser*/ window;",
+        "/*eslint-env browser*/ window;",
+        "/*eslint-env node*/ require(\"a\");",
+        "Object; isNaN();",
+        "toString()",
+        "hasOwnProperty()",
+        "function evilEval(stuffToEval) { var ultimateAnswer; ultimateAnswer = 42; eval(stuffToEval); }",
+        "typeof a",
+        "typeof (a)",
+        "var b = typeof a",
+        "typeof a === 'undefined'",
+        "if (typeof a === 'undefined') {}",
+        { code: "function foo() { var [a, b=4] = [1, 2]; return {a, b}; }", ecmaFeatures: { destructuring: true, objectLiteralShorthandProperties: true }},
+        { code: "var toString = 1;", ecmaFeatures: { globalReturn: true }},
+        { code: "function myFunc(...foo) {  return foo;}", ecmaFeatures: { restParams: true } },
+        { code: "var React, App, a=1; React.render(<App attr={a} />);", ecmaFeatures: { jsx: true } },
+        { code: "var console; [1,2,3].forEach(obj => {\n  console.log(obj);\n});", ecmaFeatures: { arrowFunctions: true } },
+        { code: "var Foo; class Bar extends Foo { constructor() { super();  }}", ecmaFeatures: { classes: true } },
+        { code: "import Warning from '../lib/warning'; var warn = new Warning('text');", ecmaFeatures: { modules: true } },
+        { code: "import * as Warning from '../lib/warning'; var warn = new Warning('text');", ecmaFeatures: { modules: true } },
+        { code: "var a; [a] = [0];", ecmaFeatures: {destructuring: true} },
+        { code: "var a; ({a}) = {};", ecmaFeatures: {destructuring: true} },
+        { code: "var a; ({b: a}) = {};", ecmaFeatures: {destructuring: true} },
+        { code: "var obj; [obj.a, obj.b] = [0, 1];", ecmaFeatures: {destructuring: true} },
+
+        // Allow decorator macro test case
+        { code: "function required() {}; var obj = { @required foo };", parser: "babel-eslint", options: [{ allowDecoratorMacro: true }]  }
+    ],
+    invalid: [
+        { code: "a = 1;", errors: [{ message: "\"a\" is not defined.", type: "Identifier"}] },
+        { code: "if (typeof anUndefinedVar === 'string') {}", options: [{typeof: true}], errors: [{ message: "\"anUndefinedVar\" is not defined.", type: "Identifier"}] },
+        { code: "var a = b;", errors: [{ message: "\"b\" is not defined.", type: "Identifier"}] },
+        { code: "function f() { b; }", errors: [{ message: "\"b\" is not defined.", type: "Identifier"}] },
+        { code: "/*global b:false*/ function f() { b = 1; }", errors: [{ message: "\"b\" is read only.", type: "Identifier"}] },
+        { code: "function f() { b = 1; }", global: { b: false }, errors: [{ message: "\"b\" is read only.", type: "Identifier"}] },
+        { code: "/*global b:false*/ function f() { b++; }", errors: [{ message: "\"b\" is read only.", type: "Identifier"}] },
+        { code: "/*global b*/ b = 1;", errors: [{ message: "\"b\" is read only.", type: "Identifier"}] },
+        { code: "window;", errors: [{ message: "\"window\" is not defined.", type: "Identifier"}] },
+        { code: "require(\"a\");", errors: [{ message: "\"require\" is not defined.", type: "Identifier"}] },
+        { code: "Array = 1;", errors: [{ message: "\"Array\" is read only.", type: "Identifier"}] },
+        { code: "var React; React.render(<img attr={a} />);", errors: [{ message: "\"a\" is not defined." }], ecmaFeatures: { jsx: true } },
+        { code: "var React, App; React.render(<App attr={a} />);", errors: [{ message: "\"a\" is not defined." }], ecmaFeatures: { jsx: true } },
+        { code: "[a] = [0];", ecmaFeatures: {destructuring: true}, errors: [{ message: "\"a\" is not defined." }] },
+        { code: "({a}) = {};", ecmaFeatures: {destructuring: true}, errors: [{ message: "\"a\" is not defined." }] },
+        { code: "({b: a}) = {};", ecmaFeatures: {destructuring: true}, errors: [{ message: "\"a\" is not defined." }] },
+        { code: "[obj.a, obj.b] = [0, 1];", ecmaFeatures: {destructuring: true}, errors: [{ message: "\"obj\" is not defined." }, { message: "\"obj\" is not defined." }] },
+
+        // Experimental
+        {
+            code: "const c = 0; const a = {...b, c};",
+            ecmaFeatures: {blockBindings: true, objectLiteralShorthandProperties: true, experimentalObjectRestSpread: true},
+            errors: [{ message: "\"b\" is not defined." }]
+        },
+
+        // Allow decorator macro test case
+        { code: "function required() {}; var obj = { @required foo };", parser: "babel-eslint", errors: [{ message: "\"foo\" is not defined." }] }
+    ]
+});
